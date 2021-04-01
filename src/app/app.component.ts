b4709b1b3373a880b2f39314ba6f2e44de670ae9
unknown <avivrazo@gmail.com>
2021-04-01 15:08:29 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dados';
+  // title = 'Dados';
+  dadoLeft = '../assets/img/dice1.png';
+  dadoRight = '../assets/img/dice3.png';
+  num1: number = 1;
+  num2: number = 2;
+  tiros: number = 0;
+
+  tirarDados(){
+    this.tiros ++;
+    this.num1 = Math.round(Math.random() * 5) + 1;
+    this.num2 = Math.round(Math.random() * 5) + 1;
+    // console.log(this.num1);
+    // console.log(this.num2);
+
+    this.dadoLeft = `../assets/img/dice${this.num1}.png`;
+    this.dadoRight = `../assets/img/dice${this.num2}.png`;
+  }
+
+  reStart(){
+    this.dadoLeft = '../assets/img/dice4.png';
+    this.dadoRight = '../assets/img/dice2.png';
+    this.num1 = 3;
+    this.num2 = 5;
+    this.tiros = 0;
+  }
+
+
 }
